2e8e3cdddde9efb77b854182a1e45e14c9ef652b
dustin4242 <thegameexposer@gmail.com>
2024-08-07 17:34:47 -0500

    added styling to blog page

--- pages/blog.vue
+++ pages/blog.vue
@@ -12,4 +12,22 @@
 .dance {
 	height: 300px;
 }
+
+.something {
+	overflow-x: scroll;
+	width: 100%;
+	height: 100%;
+}
+
+.window {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin: 10px;
+	padding: 25px;
+	border-image-source: url("/window.png");
+	border-image-slice: 37 34 5;
+	border-image-width: 37px 34px 5px 34px;
+}
 </style>
